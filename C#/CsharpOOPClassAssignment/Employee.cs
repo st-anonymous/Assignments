f0c09391a9711ff4ea8a01dc4ed9e3a671509ba3
Suryakanta Indrajit <indrajit.suryakanta.9@gmail.com>
2022-09-06 14:50:38 +0530

    C# Assignment2 Submitted

---
+++ C#/CsharpOOPClassAssignment/Employee.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpOOPClassAssignment
+{
+    internal class Employee
+    {
+        public int EmpNo { get; set; }
+        public string EmpName { get; set; }
+        public double Salary { get; set; }
+        public double HRA { get; set; }
+        public double TA { get; set; }
+        public double DA { get; set; }
+        public double PF { get; set; }
+        public double TDS { get; set; }
+        public double NetSalary { get; set; }
+        public double GrossSalary { get; set; }
+
+        public Employee(int empNo, string empName, double salary)
+        {
+            EmpNo = empNo;
+            EmpName = empName;
+            Salary = salary;
+            SetSalaryComponents(salary);
+        }
+        private void SetSalaryComponents(double salary)
+        {
+            if(salary < 5000)
+            {
+                
+                HRA = 0.1 * salary;
+                TA = 0.05 * salary;
+                DA = 0.15 * salary;
+            }
+            else if (salary < 10000)
+            {
+
+                HRA = 0.15 * salary;
+                TA = 0.1 * salary;
+                DA = 0.2 * salary;
+            }
+            else if (salary < 15000)
+            {
+
+                HRA = 0.2 * salary;
+                TA = 0.15 * salary;
+                DA = 0.25 * salary;
+            }
+            else if (salary < 20000)
+            {
+
+                HRA = 0.25 * salary;
+                TA = 0.2 * salary;
+                DA = 0.30 * salary;
+            }
+            else if (salary >= 20000)
+            {
+
+                HRA = 0.3 * salary;
+                TA = 0.25 * salary;
+                DA = 0.35 * salary;
+            }
+
+            GrossSalary = Salary + HRA + TA + DA;
+            CalculateSalary(GrossSalary);
+        }
+        private void CalculateSalary(double grossSalary)
+        {
+            PF = 0.1 * grossSalary;
+            TDS = 0.18 * grossSalary;
+            NetSalary = grossSalary - (PF + TDS);
+        }
+
+        public string ShowGrossSalary()
+        {
+            return ($" Employee Number: {EmpNo}\n Employee Name: {EmpName}\n Gross Salary: {GrossSalary}");
+        }
+
+    }
+}
---
+++ C#/CsharpOOPClassAssignment/EmployeeManagementSystem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpOOPClassAssignment
+{
+    internal class EmployeeManagementSystem
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            while (true)
+            {
+                Console.Write("\n 1. Add Employee\n 2. Show Salary\n 0. Quit\n Choose Your Action: ");
+                int opt = Convert.ToInt32(Console.ReadLine());
+                if (opt == 0) break;
+                if (opt == 1)
+                {
+                    int empNo;
+                    string empName;
+                    double salary;
+                    Console.Write("Enter Employee Number: ");
+                    empNo = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter Employee Name: ");
+                    empName = Convert.ToString(Console.ReadLine());
+                    Console.Write("Enter salary: ");
+                    salary = Convert.ToDouble(Console.ReadLine());
+                    Employee emp = new Employee(empNo, empName, salary);
+                    employees.Add(emp);
+                }
+                if (opt == 2)
+                {
+                    foreach (Employee emp in employees)
+                    {
+                        Console.WriteLine("\n" + emp.ShowGrossSalary());
+                    }
+                }
+            }
+            
+        }
+    }
+}
